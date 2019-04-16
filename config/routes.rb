b996bc169083e2a0cf050b5f5bc2693971ef9fea
Cully-Curwen <c.t.g.curwen@gmail.com>
2019-04-16 11:57:13 +0100

    Routes with resources added

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :card_details
+  resources :paypal_details
+  resources :bank_details
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
