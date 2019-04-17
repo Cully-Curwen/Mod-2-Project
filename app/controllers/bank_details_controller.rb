74b3893fa75994b8ef4da0d3ba1c9d7b31df9e19
Cully-Curwen <c.t.g.curwen@gmail.com>
2019-04-17 11:40:33 +0100

    Bank Details Controller to edit

--- app/controllers/bank_details_controller.rb
+++ app/controllers/bank_details_controller.rb
@@ -1,2 +1,8 @@
 class BankDetailsController < ApplicationController
+
+  def edit
+    # secure logic to stop other user edditing
+    @bank_details = @current_user.bank_details.select{|e| e.id == params[:id] }
+  end
+
 end
