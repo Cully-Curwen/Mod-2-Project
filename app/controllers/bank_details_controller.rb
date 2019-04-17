a979a6179f84b1082825415852207bc24cc3b7df
Cully-Curwen <c.t.g.curwen@gmail.com>
2019-04-17 12:28:07 +0100

    Bank Details can be created and edited

--- app/controllers/bank_details_controller.rb
+++ app/controllers/bank_details_controller.rb
@@ -6,7 +6,20 @@ class BankDetailsController < ApplicationController
 
   def edit
     # secure logic to stop other user edditing
-    @bank_details = @current_user.bank_details.select{|e| e.id == params[:id] }
+    @bank_details = BankDetail.where(id: params[:id], user_id: @current_user.id).first!
+  end
+
+  def update
+    bank_details = BankDetail.where(id: params[:id], user_id: @current_user.id).first!
+    bank_details.update(params_bank_details)
+
+    redirect_to controller: 'payment_method'
+  end
+
+  private
+
+  def params_bank_details
+    params.require(:bank_detail).permit(:id, :user_id, :account_holder_name, :account_name, :bank_name)
   end
 
 end
