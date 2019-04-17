04f74f493f69db4187513e694583c077d0e3285c
Fernando <fernando_lopezc@live.com>
2019-04-17 14:45:20 +0100

    hello

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   resources :bank_details
   get "/login_form", to: "sessions#login_form"
   post "/login", to: "sessions#login", as: "login"
-  get "/profile", to: "users#profile"
+  delete "/logout", to: "sessions#logout"
+  # get "/profile", to: "users#show"  as:  "profile"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
