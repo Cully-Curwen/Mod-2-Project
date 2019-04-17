4bebc27f2293d409a215e38939d9bde67cf5debf
Cully-Curwen <c.t.g.curwen@gmail.com>
2019-04-17 14:39:56 +0100

    new seed to be able to fill in subscription form

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,79 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-10.times do(
-    User.create(
-        first_name: Faker::Name.first_name,
-        last_name: Faker::Name.last_name,
-        email: Faker::Internet.email,
-        password: "123"
-    )
+User.create(
+    first_name: "Mr",
+    last_name: "Test",
+    email: "email@email",
+    password: "pass"
 )
-    end
+User.create(
+    first_name: "Dr",
+    last_name: "No",
+    email: "dr@no",
+    password: "pass"
+)
+
+BankDetail.create(
+    user_id: 1,
+    account_holder_name: "Mr Test",
+    bank_name: "To Big To Fail",
+    account_name: "Current"
+)
+BankDetail.create(
+    user_id: 1,
+    account_holder_name: "Mr Test",
+    bank_name: "High Street Money",
+    account_name: "123"
+)
+BankDetail.create(
+    user_id: 2,
+    account_holder_name: "Dr No"
+    bank_name: "To Big To Fail",
+    account_name: "Current"
+)
+
+CardDetail.create(
+    user_id: 1,
+    card_holder_name: "Mr Test", 
+    card_provider: "Plastic Money",
+    card_type: "Credit"
+)
+CardDetail.create(
+    user_id: 1,
+    card_holder_name: "Mr Test", 
+    card_provider: "Yakie Express",
+    card_type: "Credit"
+)
+CardDetail.create(
+    user_id: 2,
+    card_holder_name: "Dr No", 
+    card_provider: "VISA",
+    card_type: "Debit"
+)
+
+PaypalDetail.create(
+    user_id: 1,
+    account_email: "email@email"
+)
+
+Company.create(
+    name: "Flix",
+    company_number: "AC123452"
+)
+Company.create(
+    name: "Black Forest",
+    company_number: "39209328"
+)
+Company.create(
+    name: "Phones",
+    company_number: "BD154283"
+)
+
+Currency.create(
+    base: "GBP",
+    date: "2019-04-15",
+    rates_GBP: 1,
+    rates_USD: 1.302423,
+    rates_EUR: 1.152432
+)
