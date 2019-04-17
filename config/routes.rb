133d8446b83fdf62a6618b8016daf584f9c78813
Cully-Curwen <c.t.g.curwen@gmail.com>
2019-04-17 11:41:41 +0100

    routes for Payment menthods + buttons to edit them

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'payment_method/index'
-  get 'payment_method/new'
   resources :users
   root to: 'sessions#login_form'
   resources :subscriptions
@@ -11,5 +9,9 @@ Rails.application.routes.draw do
   get "/login_form", to: "sessions#login_form"
   post "/login", to: "sessions#login", as: "login"
   get "/profile", to: "users#profile"
+  # payment_method routes
+  get 'payment_method/', to: "payment_method#index"
+  get 'payment_method/new'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
