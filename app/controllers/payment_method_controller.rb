7a0dccd74c2a087393cc314ece7756d118ce69d8
Cully-Curwen <c.t.g.curwen@gmail.com>
2019-04-17 11:39:53 +0100

    Payment Method index page for user

--- app/controllers/payment_method_controller.rb
+++ app/controllers/payment_method_controller.rb
@@ -1,5 +1,10 @@
 class PaymentMethodController < ApplicationController
   def index
+    # user session logic
+    @bank_details = @current_user.bank_details
+    @card_details = @current_user.card_details
+    @paypal_details = @current_user.paypal_details
+    # byebug
   end
 
   def new
