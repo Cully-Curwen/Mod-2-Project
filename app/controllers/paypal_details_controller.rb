37c20ae79a1504eb72f1da59e7f1e6019308c6de
Cully-Curwen <c.t.g.curwen@gmail.com>
2019-04-17 11:41:04 +0100

    paypal controller to edit

--- app/controllers/paypal_details_controller.rb
+++ app/controllers/paypal_details_controller.rb
@@ -1,2 +1,8 @@
 class PaypalDetailsController < ApplicationController
+
+  def edit
+    # secure logic to stop other user edditing
+    @paypal_details = @current_user.bank_details.select{|e| e.id == params[:id] }
+  end
+
 end
