1ebf13729160be8a701c8403674addda531f5f4a
Cully-Curwen <c.t.g.curwen@gmail.com>
2019-04-17 13:13:08 +0100

    Create and edit Card Details

--- app/controllers/card_details_controller.rb
+++ app/controllers/card_details_controller.rb
@@ -6,7 +6,20 @@ class CardDetailsController < ApplicationController
 
   def edit
     # secure logic to stop other user edditing
-    @card_details = @current_user.bank_details.select{|e| e.id == params[:id] }
+    @card_details = CardDetail.where(id: params[:id], user_id: @current_user.id).first!
+  end
+
+  def update
+    card_details = CardDetail.where(id: params[:id], user_id: @current_user.id).first!
+    card_details.update(params_card_details)
+
+    redirect_to controller: 'payment_method'
+  end
+
+  private
+
+  def params_card_details
+    params.require(:card_detail).permit(:id, :user_id, :card_holder_name, :card_provider, :card_type)
   end
 
 end
