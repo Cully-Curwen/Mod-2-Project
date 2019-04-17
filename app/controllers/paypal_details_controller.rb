d34d34cfa18398d48a4a676ce06c169f3f43983c
Cully-Curwen <c.t.g.curwen@gmail.com>
2019-04-17 13:18:27 +0100

    Can create new and edit paypal details

--- app/controllers/paypal_details_controller.rb
+++ app/controllers/paypal_details_controller.rb
@@ -3,10 +3,23 @@ class PaypalDetailsController < ApplicationController
   def new
     @paypal_details = PaypalDetail.new
   end
-  
+
   def edit
     # secure logic to stop other user edditing
-    @paypal_details = @current_user.bank_details.select{|e| e.id == params[:id] }
+    @paypal_details = PaypalDetail.where(id: params[:id], user_id: @current_user.id).first!
+  end
+
+  def update
+    paypal_details = PaypalDetail.where(id: params[:id], user_id: @current_user.id).first!
+    paypal_details.update(params_paypal_details)
+
+    redirect_to controller: 'payment_method'
+  end
+
+  private
+
+  def params_paypal_details
+    params.require(:paypal_detail).permit(:id, :user_id, :account_email)
   end
 
 end
